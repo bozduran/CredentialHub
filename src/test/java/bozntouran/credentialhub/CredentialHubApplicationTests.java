71d9244444718adac2ffbab931b30428f65dd316
boz_ntouran <duris2009@windowslive.com>
2025-07-18 19:53:50 +0300

    Initial commit

---
+++ src/test/java/bozntouran/credentialhub/CredentialHubApplicationTests.java
@@ -0,0 +1,13 @@
+package bozntouran.credentialhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
